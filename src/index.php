07eda60428b73e428feb5028aa5aa44dc3d08544
EC2 Default User <ec2-user@ip-172-31-55-227.ec2.internal>
2018-05-01 12:21:42 +0000

    Some comment Updateed the landing page.

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>The Simple PHP App of Pi the CICD Wizard</h1>
+                <h1>This is the new landing page. Hello world!</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
